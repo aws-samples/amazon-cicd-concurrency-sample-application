4cd977e72e88922eecf3bde297093adbc66ef980
seyong <sykang@amazon.com>
2023-05-26 19:58:14 +0900

    fix agent

--- src/main/java/com/example/concurrencysample/SingletonRepo.java
+++ src/main/java/com/example/concurrencysample/SingletonRepo.java
@@ -39,13 +39,14 @@ public class SingletonRepo {
 
             ac = new BasicAWSCredentials(ACCESS_KEY, SECERET_KEY);
 
-            dynamodb = AmazonDynamoDBClientBuilder
+            
+            System.out.println("singleton repo instance created");
+        }
+        dynamodb = AmazonDynamoDBClientBuilder
                     .standard()
             .withRegion("us-west-2")
             .withCredentials(new AWSStaticCredentialsProvider(ac) )
             .build();
-            System.out.println("singleton repo instance created");
-        }
         return singletonrepo;
     }
     public Concurrency get(int product_code){
